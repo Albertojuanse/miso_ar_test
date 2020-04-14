72a6de774949143a26271265189ed91e98476991
Albertojuanse <albertojuansebastian@gmail.com>
2020-04-14 10:49:09 +0200

    Initial Commit

---
+++ AR Kit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AR Kit
+//
+//  Created by MISO on 14/04/2020.
+//  Copyright © 2020 MISO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AR Kit/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  AR Kit
+//
+//  Created by MISO on 14/04/2020.
+//  Copyright © 2020 MISO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
+
