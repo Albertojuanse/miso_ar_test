f51a7c463a1df3db9795547eeb9d41b220ddc85b
Albertojuanse <albertojuansebastian@gmail.com>
2020-05-24 23:38:08 +0200

    Basic app with debug information

---
+++ ARKit_test/ARKit_test/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ARKit_test
+//
+//  Created by MISO on 24/05/2020.
+//  Copyright © 2020 MISO. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.sceneView.debugOptions = [
+            ARSCNDebugOptions.showFeaturePoints,
+            ARSCNDebugOptions.showWorldOrigin
+        ]
+        self.sceneView.session.run(configuration)
+    }
+
+
+}
+
