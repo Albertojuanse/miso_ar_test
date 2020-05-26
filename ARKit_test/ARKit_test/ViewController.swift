5cb4103222de9c8339ef6c989c78f7e2381a9f52
Albertojuanse <albertojuansebastian@gmail.com>
2020-05-26 22:21:12 +0200

    shapes

--- ARKit_test/ARKit_test/ViewController.swift
+++ ARKit_test/ARKit_test/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
 
     @IBAction func handleTapAdd(_ sender: Any) {
         let node = SCNNode()
+        
+        /* Box or sphere (or SCNSphere); random
         node.geometry = SCNBox(width: 0.1,
                                height: 0.1,
                                length: 0.1,
@@ -36,6 +38,60 @@ class ViewController: UIViewController {
         let y = randomNumbers(firstNum: -0.3, secondNum: 0.3)
         let z = randomNumbers(firstNum: -0.3, secondNum: 0.3)
         node.position = SCNVector3(x, y, z)
+         */
+        /* Capsule
+        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.position = SCNVector3(0, 0, -0.3)
+         */
+        /* Cone or cilinder (or SCNCylinder)
+        node.geometry = SCNCone(topRadius: 0.1, bottomRadius: 0.3, height: 0.3)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.position = SCNVector3(0, 0, -0.3)
+         */
+        /* Tube
+        node.geometry = SCNTube(
+            innerRadius: 0.2,
+            outerRadius: 0.3,
+            height: 0.5)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.position = SCNVector3(0, 0, -0.3)
+         */
+        /* Torus
+        node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.position = SCNVector3(0, 0, -0.3)
+         */
+        /* Plane
+        node.geometry = SCNPlane(width: 0.2, height: 0.2)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.position = SCNVector3(0, 0, -0.3)
+         */
+        /* Pyramid
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.position = SCNVector3(0, 0, -0.3)
+         */
+        /* Bezier
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+        path.addLine(to: CGPoint(x: 0.4, y: 0))
+        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+        node.geometry = shape;
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        node.position = SCNVector3(0, 0, -0.3)
+         */
+        
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     
