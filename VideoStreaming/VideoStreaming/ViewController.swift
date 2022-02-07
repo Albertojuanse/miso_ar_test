dced0a5559db27f595910ceefd0ab2b08c49db7b
Superrub1997 <rubencampos.97@gmail.com>
2022-02-07 09:36:14 +0100

    add new buttons play and pause

---
+++ VideoStreaming/VideoStreaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoStreaming
+//
+//  Created by MISO on 26/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
