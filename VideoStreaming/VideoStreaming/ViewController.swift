ae12abc75c121677ad25bb8d99c4c5da8e26ec0b
Superrub1997 <rubencampos.97@gmail.com>
2022-02-22 15:42:05 +0100

    Update Tables

--- VideoStreaming/VideoStreaming/ViewController.swift
+++ VideoStreaming/VideoStreaming/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func playVideo(_ sender: AnyObject) {
+        guard let url = URL(string: "https://post-its-server.herokuapp.com/getMaterial.mp4") else {
+            return
+        }
+        let player = AVPlayer(url: url)
+        
+        let controller = AVPlayerViewController()
+        controller.player = player
+        
+        present(controller, animated: true) {
+            player.play()
+        }
     }
-
-
 }
 
